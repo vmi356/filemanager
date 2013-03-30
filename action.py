10fe3e716fb296df42815a3d5fd7dcebda93c491
postindustrial <victorlegotin@gmail.com>
2013-03-30 09:04:48 +0600

    Update action.py
    added file search function

--- action.py
+++ action.py
@@ -13,3 +13,12 @@ class View(Action):
         text = open(os.path.join(self.node.root, self.node.path)).read()
         context = { 'text' : text }
         return context
+        
+class Search(Action):
+    def apply(self, folder_to_search_into, mask):
+        founds = []
+        for root, dirs, files in os.walk(folder_to_search_into, topdown=False):
+            for name in files:
+                if mask in name:
+                    founds.append(os.path.join(root, name))
+        return founds
